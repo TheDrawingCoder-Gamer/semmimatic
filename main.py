8229d85bfae8a25da843edfacf97bc6638cf52ff
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-04-25 20:49:43 -0400

    log new semmi messages

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ with open("semmi.txt") as text_file:
 
 
 
-
+semmi_id = 415825875146375168
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot("$", intents=intents)
+bot = commands.Bot("s$", intents=intents)
 tree = bot.tree
 
 @bot.command()
@@ -26,10 +26,28 @@ async def sync(ctx):
 async def on_ready():
     await tree.sync()
 
-@tree.command(description="Generates semmi text.")
+@bot.hybrid_command(description="Generates semmi text.")
 async def semmimatic(ctx):
     res = model.make_sentence(tries=100)
-    await ctx.response.send_message(res)
+    await ctx.send(res)
+
+@bot.hybrid_command(description="Reloads bot model")
+@commands.is_owner()
+async def reload(ctx):
+    with open("semmi.txt") as text_file:
+        model = markovify.NewlineText(text_file.read())
+    await ctx.send("Reloaded model")
+
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if message.author.id == semmi_id:
+        with open("semmi.txt", "w") as text_file:
+            text_file.write(message.content)
+            text_file.write("\r\r\r\n")
+
 
 
 
