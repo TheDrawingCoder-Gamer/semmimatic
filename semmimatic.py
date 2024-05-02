851661a7185b842faf0fbbfeb515e3bc96c0389e
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-05-02 12:38:47 -0400

    lock file to prevent sadness

--- semmimatic.py
+++ semmimatic.py
@@ -1,4 +1,5 @@
 import markovify
+import portalocker
 
 # i'm a sinner
 class Semmimatic:
@@ -6,13 +7,13 @@ class Semmimatic:
         self.quote_path = quote_path
         self.build_model(newline)
     def build_model(self, newline=True):
-        with open(self.quote_path) as text_file:
+        with portalocker.Lock(self.quote_path, "r") as text_file:
             if newline:
                 self.model = markovify.NewlineText(text_file.read())
             else:
                 self.model = ModelText(text_file.read())
     def add_message(self, content):
-        with open(self.quote_path, "a") as text_file:
+        with portalocker.Lock(self.quote_path, "a") as text_file:
             text_file.write(message.content)
             text_file.write("\r\r\r\n")
 
